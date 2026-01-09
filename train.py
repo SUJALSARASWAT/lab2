0f60d6b36f74c8a3c031c8ff47328f16d6227e99
SUJALSARASWAT <rajeevsujal75@gmail.com>
2026-01-09 21:17:33 +0530

    Model-Ridge alpha-1.0 test-0.2 standard-scaler

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 import joblib
 import os
@@ -23,7 +23,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Model
-model = Lasso(alpha=0.1)
+model = Ridge(alpha=1.0)
 model.fit(X_train, y_train)
 
 # Prediction
